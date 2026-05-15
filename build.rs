16a58e3fccb8f075a148fd5568d690b21e51769a
Cyrus-Gahatraj <cyrusgahatraj301@gmail.com>
2026-05-15 08:46:30 +0545

    update build.rs

--- build.rs
+++ build.rs
@@ -3,29 +3,31 @@ use std::path::Path;
 
 fn main() {
     let gperf_input = "src/internal/keyword_lookup.gperf";
-    let gperf_ouput = "src/internal/keyword_lookup.c";
+    let gperf_output = "src/internal/keyword_lookup.c";
 
     if Path::new(gperf_input).exists() {
         let status = Command::new("gperf")
+            .arg("--output-file")
+            .arg(gperf_output)
             .arg("-L")
-            .arg("C")
-            .arg("-t")
-            .arg("-C")
+            .arg("ANSI-C") // modern function prototypes
+            .arg("-t")     // Includes  struct definition
+            .arg("-C")     // Makes the lookup tables 'const' (better for performance)
             .arg("-N")
             .arg("lookup_keyword")
             .arg(gperf_input)
             .output()
-            .expect("Failed to execute gperf, please make sure it is install");
+            .expect("Failed to execute gperf");
 
-        std::fs::write(gperf_ouput, status.stdout)
+        std::fs::write(gperf_output, status.stdout)
             .expect("Failed to write generated lookup code.");
     }
     println!("cargo:rerun-if-change={}", gperf_input);
 
     println!("cargo:rerun-if-changed=src/c/");
     cc::Build::new()
+        .include("src")
         .include("include")
-        .include("include/private")
         .file("src/c/memory.c") 
         .file("src/c/lexer.c") 
         .file("src/c/compiler.c") 
